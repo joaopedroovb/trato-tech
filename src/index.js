b2617f257599af87b6231143a0cd1947b9bb978f
João Pedro Oliveira <joliveira@realtec.com.br>
2022-10-12 11:47:53 -0300

    navbar quase pronto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Navbar from './components/Navbar';
 import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>Olá</div>
+    <Navbar />
   </React.StrictMode>
 );
