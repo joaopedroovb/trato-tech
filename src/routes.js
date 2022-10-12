84931fdb6b5806804bb9a6711eb42c877218d005
João Pedro Oliveira <joliveira@realtec.com.br>
2022-10-12 13:40:36 -0300

    adiciona home em router

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import PaginaPadrao from 'components/PaginaPadrao';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from 'pages/Home';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<PaginaPadrao />}>
-          <Route index element={<div>home</div>} />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
