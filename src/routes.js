453e13096795b4b7dda6ed61506704f8ae6b2591
João Pedro Oliveira <joliveira@realtec.com.br>
2022-10-12 15:49:00 -0300

    adiciona rota categoria em router

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import PaginaPadrao from 'components/PaginaPadrao';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from 'pages/Home';
+import Categoria from 'pages/Categoria';
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
       <Routes>
         <Route path='/' element={<PaginaPadrao />}>
           <Route index element={<Home />} />
+          <Route path='/categoria/:nomeCategoria' element={<Categoria />} />
         </Route>
       </Routes>
     </BrowserRouter>
